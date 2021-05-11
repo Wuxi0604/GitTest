6143e5e864bf0538cf6b9f4d8623692e0577f7b8
Wuxi0604 <65858409+Wuxi0604@users.noreply.github.com>
2021-05-11 23:12:57 +0800

    pull test master

--- src/main/java/com/sys/git/GitTest.java
+++ src/main/java/com/sys/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("push test");
         System.out.println("push test");
-
+        System.out.println("pull test");
     }
 }
